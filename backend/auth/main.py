f183791e3815baab96c47cdcde40aa7306dcded3
noa987 <noa.mourareau@gmail.com>
2023-03-01 16:17:56 +0100

    maj backend/auth

--- backend/auth/main.py
+++ backend/auth/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +12,13 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("backend/auth/{name}")
+async def connect(name: str):
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
