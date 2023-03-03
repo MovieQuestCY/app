eed63830324a2e4522acca142141996b650a1a47
Dorian Mailhe <mailhedori@eisti.eu>
2023-03-03 15:03:10 +0100

    quick testing of gh actions

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -5,7 +5,7 @@
             <h1 class="text-6xl font-extrabold">Find the perfect movie to watch together</h1>
         </div>
 
-        <input type="text" class="w-2/3 mx-auto mt-10 p-2 border-2 border-gray-300 rounded-lg" placeholder="Search for a movie" />
+        <input type="text" class="block w-2/3 mx-auto mt-10 p-2 border-2 border-gray-300 rounded-lg" placeholder="Search for a movie" />
     </section>
 </template>
 
