ec546323e304a49363fece9c7adbcad714363d1d
Esteban <58557770+estebanneuu@users.noreply.github.com>
2023-03-03 15:17:14 +0100

    Another quick update for GH Actions tests

--- frontend/src/pages/About.vue
+++ frontend/src/pages/About.vue
@@ -2,7 +2,7 @@
     <section class="container">
         <div class="pt-20 text-center">
             <h2 class="text-3xl">MovieQuest</h2>
-            <h1 class="text-7xl">About our project</h1>
+            <h1 class="text-7xl">About our great project</h1>
         </div>
     </section>
 </template>
