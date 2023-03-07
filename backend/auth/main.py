1beea8a0118524e948bd1a6088082348a9aeddc0
noa987 <noa.mourareau@gmail.com>
2023-03-07 15:33:46 +0100

    new routes connect

--- backend/auth/main.py
+++ backend/auth/main.py
@@ -1,22 +1,26 @@
 from fastapi import FastAPI
 import uvicorn
+from sqlalchemy import create_engine
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("backend/auth/connect/{email}")
+async def connectUser(email: str, password: str):
+    if(password != "password"):
+        return {"message": "Invalid password"}
+    else:
+        return {"message": f"Welcome {email}"}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("backend/auth/newUser/{email}")
+async def newUser(email: str, password: str):
+    #insert into database
+    return {"message": f"Welcome {email}"}
 
-@app.get("backend/auth/{name}")
-async def connect(name: str):
-    return {"message": f"Hello {name}"}
-
+engine = create_engine("mysql+pymysql://root:p4ssw0rd@db.neuvy.eu:3306?charset=utf8mb4")
 
 if __name__ == "__main__":
     config = uvicorn.Config("main:app", port=5000, log_level="info")
