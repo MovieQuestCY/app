8e3f5e333e24592aea6f88ff93c31b887d61bc70
rlecuppe <lecupperob@cy-tech.fr>
2023-04-03 14:49:14 +0200

    Movie API with sqlalchemy

---
+++ backend/movies/app/models/sqlalchemy_schemas.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from typing import List, Optional
+from sqlalchemy import String, Column, Table, ForeignKey
+from sqlalchemy.orm import Mapped, relationship, mapped_column, DeclarativeBase
+
+class Base(DeclarativeBase):
+    """The base class for all models"""
+    pass
+
+class User(Base):
+    """The sqlalchemy movie model, used to create the database 'movies' table"""
+    __tablename__ = "movies"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(250))
+    year: Mapped[int] = mapped_column(int)
+    genre: Mapped[List[str]] = mapped_column(String(50))
+    director: Mapped[str] = mapped_column(String(250))
+
+    def __str__(self) -> str:
+        return f"Movie(title={self.title}, year={self.year}, genre={self.genre}, director={self.director})"
+
---
+++ backend/movies/app/routes/movies.py
@@ -0,0 +1,61 @@
+from typing import List
+from fastapi import Depends, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+from controllers.movies import create_movie, get_movie, get_movies, get_movie_by_title, delete_movie, edit_movie
+from models.pydantic_schemas import Movie, MovieCreate
+from db import SessionLocal
+
+movie_router = APIRouter(prefix="/movies")
+
+# Dependency
+def get_db():
+    """Get a database session"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#TODO : Find another point to check if the movie already exists
+@movie_router.post("/", response_model=Movie)
+def create_movie_route(movie: MovieCreate, db: Session = Depends(get_db)) -> Movie:
+    db_movie = get_movie_by_title(db, title=movie.title)
+    if db_movie:
+        raise HTTPException(status_code=400, detail="Title already registered")
+    return create_movie(db=db, movie=movie)
+
+@movie_router.get("/{movie_id}", response_model=Movie)
+def read_movie_route(user_id: int, db: Session = Depends(get_db)) -> Movie:
+    db_movie = get_movie(db, movie_id=movie_id)
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return db_movie
+
+@movie_router.get("/", response_model=List[Movie])
+def read_movies_route(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> List[Movie]:
+    movies = get_movies(db, skip=skip, limit=limit)
+    return movies
+
+@movie_router.get("/by_title/{title}", response_model=Movie)
+def read_movie_by_title_route(title: str, db: Session = Depends(get_db)) -> Movie:
+    db_movie = get_movie_by_title(db, title=title)
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return db_movie
+
+@movie_router.delete("/{movie_id}", response_model=Movie)
+def delete_movie_route(movie_id: int, db: Session = Depends(get_db)) -> Movie:
+    db_movie = get_movie(db, movie_id=movie_id)
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return delete_movie(db, movie_id=movie_id)
+
+@movie_router.put("/{movie_id}", response_model=Movie)
+def edit_movie_route(movie_id: int, movie: MovieCreate, db: Session = Depends(get_db)) -> Movie:
+    db_movie = get_movie(db, movie_id=movie_id)
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return edit_movie(db, movie_id=movie_id, movie=movie)
+
+
+
