54725b7f13f60ca290a8281ba90707b08fb0e879
noa987 <noa.mourareau@gmail.com>
2023-04-26 11:26:17 +0200

    package bdd

---
+++ backend/bdd_package/Team.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from pydantic import BaseModel
+
+class TeamBase(BaseModel):
+    """The base team model"""
+    name: str
+
+class TeamCreate(TeamBase):
+    pass
+
+class Team(TeamBase):
+    """The team model for returning a team"""
+    id: int
+
+    class Config:
+        """The config for the team model"""
+        orm_mode = True
