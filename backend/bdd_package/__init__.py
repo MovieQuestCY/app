6aa11e74f7d180c5be46251edd030c00132c3fc7
noa987 <noa.mourareau@gmail.com>
2023-04-26 12:43:25 +0200

    module

--- backend/bdd_package/__init__.py
+++ backend/bdd_package/__init__.py
@@ -1,3 +1,4 @@
-from .Movie import Movie, MovieCreate, MovieBase
-from .User import User, UserCreate, UserBase, UserLogin, UserLogged
-from .Team import Team, TeamCreate, TeamBase
+from .pydantic.Movie import Movie, MovieCreate, MovieBase
+from .pydantic.User import User, UserCreate, UserBase, UserLogin, UserLogged
+from .pydantic.Team import Team, TeamCreate, TeamBase
+from .sqlalchemy.schemas import Movie, User, Team, has_team, has_watched
