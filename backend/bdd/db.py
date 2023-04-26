030091ebb4c63857ef612f7ad765b9b1d07f34d4
Esteban <58557770+estebanneuu@users.noreply.github.com>
2023-04-26 15:02:57 +0200

    Update bdd path

--- backend/bdd/db.py
+++ backend/bdd/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://fastapi:fastapi@localhost/moviequest"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://fastapi:fastapi@mysql/moviequest"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
